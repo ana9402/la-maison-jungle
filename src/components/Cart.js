329d68d70b55ac00e3b6a8d5951c2aafa963d321
Anaïs <anais75.b@gmail.com>
2021-11-05 16:43:03 +0100

    basic components

---
+++ src/components/Cart.js
@@ -0,0 +1,20 @@
+import '../styles/Cart.css'
+
+function Cart() {
+    const monsteraPrice = 8;
+    const ivyPrice = 10;
+    const flowerPrice = 15;
+    return (
+        <div className='lmj-cart'>
+            <h2>Panier</h2>
+            <ul>
+                <li>Monstera : { monsteraPrice }€</li>
+                <li>Lierre : { ivyPrice }€</li>
+                <li>Bouquet : { flowerPrice }€</li>
+            </ul>
+            <p>Total : { monsteraPrice + ivyPrice + flowerPrice}€</p>
+        </div>
+    )
+}
+
+export default Cart
